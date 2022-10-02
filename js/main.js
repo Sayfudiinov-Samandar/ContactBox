8777cd5f411f25b52758a1f45c8b68893904d6db
sayfutdiinov <sayfutdinovsaman@gmail.com>
2022-10-02 18:24:56 +0500

    Added new method save contact books

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ let elUserName = document.querySelector(".user-name");
 let elUserNickName = document.querySelector(".user-nickname");
 let elUserNumber = document.querySelector(".user-number");
 let elResultBox = document.querySelector(".result-list");
-let usersArray = []
+
+let usersArray = JSON.parse(localStorage.getItem('contactArray')) || [];
+
 console.log(usersArray);
 
 elForm.addEventListener("submit", function (evt) {
@@ -23,31 +25,37 @@ elForm.addEventListener("submit", function (evt) {
         }
         usersArray.push(obj);
     }
-        elResultBox.innerHTML = ""
-
-        for (const userifoBox of usersArray) {
-
-            const listItem = document.createElement("li");
-            const listItemName = document.createElement("p");
-            const listItemNick = document.createElement("p");
-            const listItemNumber = document.createElement("a");
-
-            listItem.className = 'bg-white p-3 col-3 rounded'
-            listItemNumber.className='text-decoration-none text-primary'
-
-            elResultBox.appendChild(listItem);
-            listItem.appendChild(listItemName);
-            listItem.appendChild(listItemNick);
-            listItem.appendChild(listItemNumber);
-
-            listItemName.textContent = "Name: " + userifoBox.name;
-            listItemNick.textContent = "Nick name: " + userifoBox.nickname;
-            listItemNumber.href = "tel:+998" + userifoBox.number;
-            listItemNumber.textContent = "Tel +998 " + userifoBox.number
-
-        }
-
+    makeContactbBox(usersArray);
+    localStorage.setItem('contactArray', JSON.stringify(usersArray));
     elUserName.value = ""
     elUserNickName.value = ""
     elUserNumber.value = ""
 })
+
+function makeContactbBox(item) {
+    elResultBox.innerHTML = ""
+    item.forEach(i => {
+
+        const listItem = document.createElement("li");
+        const listItemName = document.createElement("p");
+        const listItemNick = document.createElement("p");
+        const listItemNumber = document.createElement("a");
+
+        listItem.className = 'bg-white p-3 col-3 rounded'
+        listItemNumber.className = 'text-decoration-none text-primary'
+
+        elResultBox.appendChild(listItem);
+        listItem.appendChild(listItemName);
+        listItem.appendChild(listItemNick);
+        listItem.appendChild(listItemNumber);
+
+        listItemName.textContent = "Name: " + i.name;
+        listItemNick.textContent = "Nick name: " + i.nickname;
+        listItemNumber.href = "tel:+998" + i.number;
+        listItemNumber.textContent = "Tel +998 " + i.number
+
+    });
+
+};
+
+makeContactbBox(usersArray);
