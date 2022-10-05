d01b1d51969ea179376198bd7f8d86706db7463b
sayfutdiinov <sayfutdinovsaman@gmail.com>
2022-10-05 06:47:32 +0500

    Added new functions

--- js/main.js
+++ js/main.js
@@ -3,59 +3,68 @@ let elUserName = document.querySelector(".user-name");
 let elUserNickName = document.querySelector(".user-nickname");
 let elUserNumber = document.querySelector(".user-number");
 let elResultBox = document.querySelector(".result-list");
+let elRsultFthBox=document.querySelector(".result-box");
+let elTemplate = document.querySelector(".contact_template").content;
+let usersArray = JSON.parse(window.localStorage.getItem("array")) || [];
 
-let usersArray = JSON.parse(localStorage.getItem('contactArray')) || [];
+let fragment = new DocumentFragment();
 
-console.log(usersArray);
 
 elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
+    
     let nameValue = elUserName.value;
     let nickValue = elUserNickName.value;
     let numberValue = Number(elUserNumber.value);
 
-    if (nameValue == "" || nickValue == "" || numberValue == "") {
-        alert("You must write full info");
-    } else {
-        let obj = {
-            id: usersArray.length + 1,
-            name: nameValue,
-            nickname: nickValue,
-            number: numberValue
-        }
-        usersArray.push(obj);
+    let obj = {
+        id: usersArray.length + 1,
+        name: nameValue,
+        nickname: nickValue,
+        number: numberValue,
+        isCompLate: false,
     }
-    makeContactbBox(usersArray);
-    localStorage.setItem('contactArray', JSON.stringify(usersArray));
+
+    if (elUserName.value!=""){
+        usersArray.push(obj);
+    }else{
+        alert("To'liq malumot bering!")
+    }
+    
+
+
+    window.localStorage.setItem("array",JSON.stringify(usersArray))
+    makeContactbBox();
     elUserName.value = ""
     elUserNickName.value = ""
     elUserNumber.value = ""
 })
 
-function makeContactbBox(item) {
+
+//function
+function makeContactbBox() {
     elResultBox.innerHTML = ""
-    item.forEach(i => {
 
-        const listItem = document.createElement("li");
-        const listItemName = document.createElement("p");
-        const listItemNick = document.createElement("p");
-        const listItemNumber = document.createElement("a");
-
-        listItem.className = 'bg-white p-3 col-3 rounded'
-        listItemNumber.className = 'text-decoration-none text-primary'
-
-        elResultBox.appendChild(listItem);
-        listItem.appendChild(listItemName);
-        listItem.appendChild(listItemNick);
-        listItem.appendChild(listItemNumber);
-
-        listItemName.textContent = "Name: " + i.name;
-        listItemNick.textContent = "Nick name: " + i.nickname;
-        listItemNumber.href = "tel:+998" + i.number;
-        listItemNumber.textContent = "Tel +998 " + i.number
-
+    usersArray.forEach(i => {
+        let templateClone = elTemplate.cloneNode(true);
+        templateClone.querySelector(".listItemName").textContent = "Name: " + i.name;
+        templateClone.querySelector(".listItemNick").textContent = "Nick: " + i.nickname;
+        templateClone.querySelector(".listItemNumber").textContent = "Number: " + i.number;
+        templateClone.querySelector(".listDeleteContact").textContent= "Delete: "
+        templateClone.querySelector(".listDeleteContact").dataset.arrayPlace=i.id
+        fragment.appendChild(templateClone);        
     });
-
+    elResultBox.appendChild(fragment)
 };
 
-makeContactbBox(usersArray);
+elRsultFthBox.addEventListener("click",function(evt){
+    if (evt.target.matches(".listDeleteContact")) {
+        let btnId = Number(evt.target.dataset.arrayPlace);
+        let arrayItemIndex = usersArray.findIndex(i=>i.id === btnId);
+        usersArray.splice(arrayItemIndex, 1);
+        window.localStorage.setItem("array",JSON.stringify(usersArray))
+        makeContactbBox();
+    }
+})
+
+makeContactbBox();
